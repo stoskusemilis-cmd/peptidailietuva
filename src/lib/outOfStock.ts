b2b8ce8b7b807f9195eee43342088b08cca82032
Nuomos Karaliai <stoskus.emilis@gmail.com>
2026-05-31 16:53:05 +0300

    Add BPC-157 + TB-500 product with tiers

--- src/lib/outOfStock.ts
+++ src/lib/outOfStock.ts
@@ -6,6 +6,7 @@ const OUT_OF_STOCK_NAMES = new Set([
   '5-AMINO-1MQ 10MG',
   'SS-31 50MG',
   'BPC-157 10MG',
+  'BPC-157 10MG + TB-500 10MG',
 ]);
 
 export function isOutOfStock(product: { name: string; stock?: number }): boolean {
