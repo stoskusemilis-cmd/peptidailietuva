db312c56a0be967290f621caf1655b4cf692c016
Nuomos Karaliai <stoskus.emilis@gmail.com>
2026-05-31 17:47:00 +0300

    Updated outOfStock.ts

--- src/lib/outOfStock.ts
+++ src/lib/outOfStock.ts
@@ -15,6 +15,7 @@ const OUT_OF_STOCK_NAMES = new Set([
   'TIRZEPATIDE 30MG',
   'DSIP 5MG',
   'EPITALON 50MG',
+  'KPV 10MG',
 ]);
 
 export function isOutOfStock(product: { name: string; stock?: number }): boolean {
