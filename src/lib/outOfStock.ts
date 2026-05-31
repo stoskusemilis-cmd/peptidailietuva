c97273de19fbd808044b378c7b8c7235caf4296e
Nuomos Karaliai <stoskus.emilis@gmail.com>
2026-05-31 16:59:44 +0300

    Updated outOfStock.ts

--- src/lib/outOfStock.ts
+++ src/lib/outOfStock.ts
@@ -8,6 +8,7 @@ const OUT_OF_STOCK_NAMES = new Set([
   'BPC-157 10MG',
   'TB-500 10MG',
   'BPC-157 10MG + TB-500 10MG',
+  'MELANOTAN 1 10MG',
 ]);
 
 export function isOutOfStock(product: { name: string; stock?: number }): boolean {
