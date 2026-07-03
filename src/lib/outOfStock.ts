8bd9ccb55fe41eb39b1c4d93b790fac0f8ff344a
Nuomos Karaliai <stoskus.emilis@gmail.com>
2026-07-03 22:40:21 +0300

    Update product order and availability

--- src/lib/outOfStock.ts
+++ src/lib/outOfStock.ts
@@ -1,16 +1,7 @@
 const OUT_OF_STOCK_NAMES = new Set([
   'HGH 15IU',
-  'MASTERONE E 2000MG',
   '5-AMINO-1MQ 10MG',
-  'BPC-157 10MG',
   'TB-500 10MG',
-  'BPC-157 10MG + TB-500 10MG',
-  'PT-141 10MG',
-  'AOD-9604 10MG',
-  'TESAMORELIN 20MG',
-  'DSIP 5MG',
-  'EPITALON 50MG',
-  'KPV 10MG',
 ]);
 
 export function isOutOfStock(product: { name: string; stock?: number }): boolean {
